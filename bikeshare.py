be665948a6d11b3f5a9f056f8d7a86f226dff2ea
Abbey Christian Banafo <christianbanafo@gmail.com>
2022-04-19 22:57:26 +0100

    refactor: Give a much descriptive name
    A more descriptive name for the stations with most combinations

--- bikeshare.py
+++ bikeshare.py
@@ -140,7 +140,7 @@ def station_stats(df):
 
     # TO DO: display most frequent combination of start station and end station trip
 
-    Combination_Station = df.groupby(['Start Station', 'End Station']).count()
+    Most_Combined_Station = df.groupby(['Start Station', 'End Station']).count()
     print('\nMost Common combination of start and end station trip:',
           Start_Station, " & ", End_Station)
 
